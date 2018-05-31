b58375266477d477fd1740bcb53bf2ef9e301379
Alexander Llanes <alexander.llanes@nareia.com.uy>
2018-05-31 15:29:20 -0300

    Add css, svg and button styles

---
+++ XUM.Mobile/XUM.Mobile/HelloCssView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XUM.Mobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HelloCssView : ContentPage
+	{
+		public HelloCssView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ XUM.Mobile/XUM.Mobile/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XUM.Mobile
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ButtonSampleView());
+        }
+
+        async void OnHelloCssPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HelloCssView());
+        }
+
+        async void OnSvgFilesPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SvgFilesView());
+        }
+    }
+}
